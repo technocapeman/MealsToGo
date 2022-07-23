f90c5eb4da3d03f23c651d606bbfad7fc0b7f4db
Kapilesh Pennichetty <kapilesh.pennichetty@gmail.com>
2022-07-22 23:03:50 -0500

    Set up React Context and Fake API Request

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
@@ -6,34 +7,25 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 
 import { SearchContainer, RestaurantList } from "./restaurants.screen.styles";
 
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
 export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <RestaurantList
-        data={[
-          { name: 1 },
-          { name: 2 },
-          { name: 3 },
-          { name: 4 },
-          { name: 5 },
-          { name: 6 },
-          { name: 7 },
-          { name: 8 },
-          { name: 9 },
-          { name: 10 },
-          { name: 11 },
-          { name: 12 },
-          { name: 13 },
-          { name: 14 },
-        ]}
-        renderItem={() => (
-          <Spacer position="bottom" size="large">
-            <RestaurantInfoCard />
-          </Spacer>
-        )}
+        data={restaurants}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={item} />
+            </Spacer>
+          );
+        }}
         keyExtractor={(item) => item.name}
       />
     </SafeArea>
